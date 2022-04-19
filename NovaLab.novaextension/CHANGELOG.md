beb69e5bba75d5e3bb463d80733ad0d8aec3ae2d
Marco Varisco <marco@varisco.info>
2022-04-19 10:47:26 -0400

    Add Language Server interface

--- NovaLab.novaextension/CHANGELOG.md
+++ NovaLab.novaextension/CHANGELOG.md
@@ -1,3 +1,7 @@
+## Version 0.1
+
+Added Language Server interface
+
 ## Version 0.0
 
 Initial commit on GitHub
--- NovaLab.novaextension/Scripts/main.js
+++ NovaLab.novaextension/Scripts/main.js
@@ -1,12 +1,20 @@
+let langserver = null;
+
 exports.activate = function() {
     // Do work when the extension is activated
     nova.assistants.registerTaskAssistant(new NovaLabTasks(), {
         identifier: NovaLabTasks.identifier
     });
+    
+    langserver = new LangServer();
 }
 
 exports.deactivate = function() {
     // Clean up state before the extension is deactivated
+    if (langserver) {
+        langserver.deactivate();
+        langserver = null;
+    }
 }
 
 
@@ -128,3 +136,98 @@ nova.commands.register("novalab.splitextDebug", (workspace) => {
     const path = workspace.activeTextEditor?.document.path;
     return "nova.path.splitext" + (path ? (`(${path}) = ` + nova.path.splitext(path)) : "() = ");
 });
+
+
+class LangServer {
+    constructor() {
+        // Observe the configuration settings for the server, and restart the server on change
+        nova.workspace.config.observe("novalab.ls-activate", () => this.start(), this);
+        nova.workspace.config.onDidChange("novalab.ls-path", () => this.start(), this);
+        nova.workspace.config.onDidChange("novalab.ls-args", () => this.start(), this);
+        nova.workspace.config.onDidChange("novalab.ls-synt", () => this.start(), this);
+    }
+    
+    deactivate() {
+        this.stop();
+    }
+    
+    start() {
+        if (this.languageClient) {
+            this.languageClient.stop();
+            nova.subscriptions.remove(this.languageClient);
+        }
+        
+        const bool = nova.workspace.config.get("novalab.ls-activate", "boolean");
+        const path = nova.workspace.config.get("novalab.ls-path", "string")?.trim();
+        const syntaxes = getSyntaxes(nova.workspace);
+        
+        if (bool && path && syntaxes.length) {
+            const args = [
+                path,
+                ...nova.workspace.config.get("novalab.ls-args", "array")
+            ];
+            
+            console.log(
+                `Starting server "/usr/bin/env ${args.join(" ")}" for ${syntaxes.length} syntax${syntaxes.length > 1 ? "es" : ""}: ${syntaxes.map((el) => `"${el}"`).join(", ")}`
+            );
+            
+            // Create the client
+            const serverOptions = {
+                path: "/usr/bin/env",
+                args: args
+            };
+            const clientOptions = {
+                // The set of document syntaxes for which the server is valid
+                syntaxes: syntaxes
+            };
+            const client = new LanguageClient("novalab.langserver", "NovaLab Language Server", serverOptions, clientOptions);
+            
+            try {
+                // Start the client
+                client.start();
+                
+                // Add the client to the subscriptions to be cleaned up
+                nova.subscriptions.add(client);
+                this.languageClient = client;
+            }
+            catch (err) {
+                // If the .start() method throws, it’s likely because the path to the language server is invalid
+                console.error(err);
+            }
+        }
+    }
+    
+    stop() {
+        if (this.languageClient) {
+            this.languageClient.stop();
+            nova.subscriptions.remove(this.languageClient);
+            this.languageClient = null;
+        }
+    }
+}
+
+function getSyntaxes(workspace) {
+    return workspace.config.get("novalab.ls-synt", "array")
+    ?.map((element) => element.trim())
+    .filter(Boolean)
+    .filter((element, index, array) => array.indexOf(element) === index)
+    || [];
+}
+
+function setSyntaxes(workspace) {
+    const syntaxes = getSyntaxes(workspace);
+    
+    new Set(
+        workspace.textEditors
+        .map((editor) => editor.document.syntax)
+    )
+    .forEach((syntax) => {
+        if (syntax && !syntaxes.includes(syntax)) {
+            syntaxes.push(syntax);
+        }
+    });
+    
+    workspace.config.set("novalab.ls-synt", syntaxes);
+}
+
+nova.commands.register("novalab.setSyntaxes", setSyntaxes);
